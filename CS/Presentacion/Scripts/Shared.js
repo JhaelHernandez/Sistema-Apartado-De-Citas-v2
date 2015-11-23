dad04ebead64144402921e5fa994d9313a393ca7
erik jhael hernandez muñoz <kire_9191@hotmail.com>
2015-11-22 23:37:59 -0700

    ok
    asd

--- CS/Presentacion/Scripts/Shared.js
+++ CS/Presentacion/Scripts/Shared.js
@@ -13,8 +13,59 @@
 
 function GoToDiv(obj) {
     var idDiv = $(obj).attr("div");
-    $('html,body').animate({
+    $('body').animate({
         scrollTop: $(idDiv).offset().top
     },
        'slow');
 };
+
+
+
+function GoToDiv(obj) {
+    var idDiv = $(obj).attr("div");
+
+    $('html, body').stop().animate({
+
+        'scrollTop': idDiv.offset().top
+
+    }, 500, 'swing', function () {
+
+        window.location.hash = idDiv;
+
+        $(document).on("scroll", onScroll);
+
+    });
+};
+
+
+function onScroll(event){
+
+    var scrollPosition = $(document).scrollTop();
+
+    $('.nav li a').each(function () {
+
+        var currentLink = $(this);
+
+        var refElement = $(currentLink.attr("href"));
+
+        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+
+            $('.nav li a').removeClass("active");
+
+            currentLink.addClass("active");
+
+        }
+
+        else{
+
+            currentLink.removeClass("active");
+
+        }
+
+    });
+        
+
+
+
+
+
