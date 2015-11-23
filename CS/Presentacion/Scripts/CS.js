df8a5689c4f786ac2018def83ee812d5ae8dd581
erik jhael hernandez muñoz <kire_9191@hotmail.com>
2015-11-22 17:03:19 -0700

    super commit
    se volvio a subir el proyecto

---
+++ CS/Presentacion/Scripts/CS.js
@@ -0,0 +1,11 @@
+﻿$(window).scroll(function () {
+    if (window.pageYOffset >= 231) {
+        $('#header').addClass("navbar-fixed-top");
+    } else {
+        $('#header').removeClass("navbar-fixed-top");
+    }
+});
+
+
+
+$('body').addClass("showItem");
---
+++ CS/Presentacion/Scripts/Shared.js
@@ -0,0 +1,3 @@
+﻿function Transition(id) {
+    $(id).addClass("showItem");
+}
